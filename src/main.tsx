295a8aeca6398443b0631675d5bad2544a4d9e11
slopezdev1 <slopezdev1@gmail.com>
2025-02-23 18:36:08 -0300

    add headlessUi and talwind config

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/global.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
