1bb5ea39e48ff0744528b941f744adce12342bb9
slopezdev1 <slopezdev1@gmail.com>
2025-02-24 08:38:05 -0300

    fix build

--- src/services/cities.services.ts
+++ src/services/cities.services.ts
@@ -1,10 +1,9 @@
-import { ICities } from "../interfaces/cities.interface";
 import { axiosInstance } from "../utils/axios.client";
 
 
 export const getCities = async (countryName: string): Promise<any> => {
     try {
-        const response: any = await axiosInstance.post<ICities>("countries/states", {
+        const response: any = await axiosInstance.post("countries/states", {
             country: countryName
         })
         return Promise.resolve(response.data.data.states)
